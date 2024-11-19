04164e9da78266663c79bfe1ed85d4a607ae5265
Caggiano <O013781@arg.bbva.com>
2024-11-19 16:09:01 -0300

    Inicialización de proyecto

---
+++ src/main/java/com/example/bbva/squad2/Wallet/WalletApplication.java
@@ -0,0 +1,21 @@
+package com.example.bbva.squad2.Wallet;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			System.out.println("Inicio de aplicación.");
+		};
+	}
+}
