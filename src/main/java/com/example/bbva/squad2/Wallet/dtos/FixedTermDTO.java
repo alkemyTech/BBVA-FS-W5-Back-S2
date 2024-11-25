8fbef4ace50c8e5df6ea4c3d3f790e2e0bf3a0df
Caggiano <O013781@arg.bbva.com>
2024-11-25 16:54:19 -0300

    Subo funcionalidad de generar plazo fijo

--- src/main/java/com/example/bbva/squad2/Wallet/dtos/FixedTermDTO.java
+++ src/main/java/com/example/bbva/squad2/Wallet/dtos/FixedTermDTO.java
@@ -1,18 +1,28 @@
 package com.example.bbva.squad2.Wallet.dtos;
 
 import com.example.bbva.squad2.Wallet.models.FixedTermDeposit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FixedTermDTO {
     private Double amount;
     private String startDate;
     private String endDate;
     private Double interestRate;
+    private String accountCBU;
 
     public FixedTermDTO mapFromFixedTerm(FixedTermDeposit fixedTermDeposit) {
         this.amount = fixedTermDeposit.getAmount();
         this.startDate = fixedTermDeposit.getStartDate().toString();
         this.endDate = fixedTermDeposit.getEndDate() != null ? fixedTermDeposit.getEndDate().toString() : "N/A";
         this.interestRate = fixedTermDeposit.getInterestRate();
+        this.accountCBU = fixedTermDeposit.getAccount().getCbu();
         return this;
     }
 }
--- src/main/java/com/example/bbva/squad2/Wallet/repositories/FixedTermDepositRepository.java
+++ src/main/java/com/example/bbva/squad2/Wallet/repositories/FixedTermDepositRepository.java
@@ -4,9 +4,11 @@ import com.example.bbva.squad2.Wallet.models.FixedTermDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface FixedTermDepositRepository extends JpaRepository<FixedTermDeposit, Long> {
     List<FixedTermDeposit> findByAccountUserId(Long userId);
+    List<FixedTermDeposit> findByClosingDateBeforeAndProcessedFalse(LocalDateTime dateTime);
 }
