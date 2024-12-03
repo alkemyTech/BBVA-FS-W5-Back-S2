0c059ed1b5fa3d8485b782f45e0c06c4a7f5b10a
Caggiano <O013781@arg.bbva.com>
2024-12-03 16:33:24 -0300

    Arreglo en test

--- src/test/java/com/example/bbva/squad2/Wallet/controllers/FixedTermDepositControllerTest.java
+++ src/test/java/com/example/bbva/squad2/Wallet/controllers/FixedTermDepositControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import jakarta.servlet.http.HttpServletRequest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -67,27 +68,23 @@ public class FixedTermDepositControllerTest {
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         FixedTermDTO mockResponse3 = (FixedTermDTO) response.getBody();
         assertEquals(mockResponse, mockResponse3);
-//        verify(usuarioLoggeadoService, times(1)).getInfoUserSecurity(request);
-//        verify(fixedTermDepositService, times(1)).createFixedTermDeposit(1L, amount, days, false);
     }
 
     @Test
-    void testCreateFixedTermDeposit_ValidationError() {
+    void testCreateFixedTermDeposit_Error() {
         // Arrange
-        Double amount = 5000.0; // Menor a los requerimientos
-        Integer days = 20; // Menor al mínimo permitido
+        Double amount = 500.0;
+        Integer days = 25;
+        String expectedErrorMessage = "El plazo fijo debe ser de al menos 30 días.";
 
-        when(usuarioLoggeadoService.getInfoUserSecurity(request)).thenReturn(mockUser);
-        when(fixedTermDepositService.createFixedTermDeposit(1L, amount, days, false))
-                .thenThrow(new RuntimeException("El plazo fijo debe ser de al menos 30 días."));
+        when(usuarioLoggeadoService.getInfoUserSecurity(any(HttpServletRequest.class))).thenReturn(mockUser);
+        doThrow(new IllegalArgumentException(expectedErrorMessage))
+                .when(fixedTermDepositService).createFixedTermDeposit(anyLong(), anyDouble(), anyInt(), anyBoolean());
 
-        // Act
-        ResponseEntity<?> response = fixedTermDepositController.createFixedTermDeposit(amount, days, request);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fixedTermDepositController.createFixedTermDeposit(amount, days, request);
+        });
 
-        // Assert
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("El plazo fijo debe ser de al menos 30 días.", response.getBody());
-        verify(usuarioLoggeadoService, times(1)).getInfoUserSecurity(request);
-        verify(fixedTermDepositService, times(1)).createFixedTermDeposit(1L, amount, days, false);
+        assertEquals(expectedErrorMessage, exception.getMessage());
     }
 }
