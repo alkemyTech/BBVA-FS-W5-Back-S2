270d56791a6d501b74295b8a59ff8f898556dfee
Pereira <O013809@arg.bbva.com>
2024-12-19 18:14:54 -0300

    type transaction en balance

--- src/main/java/com/example/bbva/squad2/Wallet/dtos/TransactionBalanceDTO.java
+++ src/main/java/com/example/bbva/squad2/Wallet/dtos/TransactionBalanceDTO.java
@@ -2,6 +2,7 @@ package com.example.bbva.squad2.Wallet.dtos;
 
 import com.example.bbva.squad2.Wallet.enums.Concept;
 import com.example.bbva.squad2.Wallet.enums.CurrencyTypeEnum;
+import com.example.bbva.squad2.Wallet.enums.TransactionTypeEnum;
 import com.example.bbva.squad2.Wallet.models.Transaction;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +26,8 @@ public class TransactionBalanceDTO {
 
     private String description;
 
+    private TransactionTypeEnum type;
+
     public TransactionBalanceDTO mapFromTransaction(Transaction transaction) {
         this.amount = transaction.getAmount();
         this.cbuDestino = transaction.getCbuDestino();
@@ -32,6 +35,7 @@ public class TransactionBalanceDTO {
         this.currency = transaction.getAccount().getCurrency();
         this.concept = transaction.getConcept();
         this.description = transaction.getDescription();
+        this.type = transaction.getType();
         return this;
     }
 }
